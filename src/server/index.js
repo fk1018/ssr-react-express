15b4ecc29d62445e964d464c657779604333e2db
Fred Khoury <fred.khoury@alloy.digital>
2018-10-04 16:29:28 -0500

    init

---
+++ src/server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import {renderToString} from 'react-dom/server';
+import App from '../shared/App';
+import React from 'react';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static('public'));
+
+app.get('*', (req,res,next)=>{
+  const markup = renderToString(
+    <App />
+  );
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>SSR React Application</title>
+      </head>
+
+      <body>
+        <div id="app">${markup}</div>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000, ()=> {
+  console.log('Server is listening on port 3000');
+});
